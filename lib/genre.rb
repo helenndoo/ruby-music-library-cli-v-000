d0b6db1113fd5091ac512a9cc39233ac88c752b1
Helen Du <helendu.614@gmail.com>
2018-05-10 19:27:42 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -4,15 +4,11 @@ class Genre
 
   @@all = []
   attr_accessor :name
-  
+
   def initialize(name)
     @name = name
     @songs = []
   end
-  def self.all
-    @@all
-  end
-  
 
   def songs
     @songs
