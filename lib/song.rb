c45a91e7aed3d87e3e11912de6412726f95577dd
Helen Du <helendu.614@gmail.com>
2018-05-10 19:41:13 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,21 +1,23 @@
-class Song
+require 'pry'
 
-  @@all = []
+class Song
   attr_accessor :name, :artist, :genre
-  def initialize(name, artist = nil, genre = nil)
+  @@all = []
+
+  def initialize(name, artist=nil, genre=nil)
     @name = name
-    self.genre = genre if genre
-    self.artist = artist if artist
+    self.artist = artist if artist != nil
+    self.genre = genre if genre != nil
   end
 
   def artist=(artist)
     @artist = artist
-    artist.add_song(self)
+    artist.add_song(self) unless artist.songs.include?(self)
   end
 
   def genre=(genre)
     @genre = genre
-    genre.add_song(self)
+    genre.add_song(self) unless genre.songs.include?(self)
   end
 
   def self.all
@@ -23,40 +25,36 @@ class Song
   end
 
   def self.destroy_all
-    self.all.clear
+    @@all.clear
   end
 
   def save
-    self.class.all << self
+    @@all << self
   end
 
-  def self.create(name)
-    song = new(name)
-    song.save
-    song
+  def self.create(name, artist=nil, genre=nil)
+    self.new(name, artist, genre).tap {|s| s.save}
   end
 
   def self.find_by_name(name)
-    self.all.detect{|song| song.name == name}
+    @@all.detect {|song| song.name == name}
   end
 
   def self.find_or_create_by_name(name)
     self.find_by_name(name) || self.create(name)
   end
 
-  def self.new_from_filename(filename)
-    song_attributes = filename.split(" - ")
-    artist_name = song_attributes[0]
+  def self.new_from_filename(name)
+    data = name.split(" - ")
+    artist_name, song_name, genre_name = data[0], data[1], data[2].gsub(".mp3", "")
     artist = Artist.find_or_create_by_name(artist_name)
-    song_name = song_attributes[1]
-    genre_name = song_attributes[2].chomp(".mp3")
     genre = Genre.find_or_create_by_name(genre_name)
-    new_song = self.new(song_name, artist, genre)
+    self.create(song_name, artist, genre)
   end
 
-  def self.create_from_filename(filename)
-    song = self.new_from_filename(filename)
-    song.save
-    song
+  def self.create_from_filename(data)
+    self.new_from_filename(data)
   end
+
+
 end
